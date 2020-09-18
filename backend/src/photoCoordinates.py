f1dd0cddd9930ba1b343d7ee6198d85e795f636c
himol7 <himolms@gmail.com>
2020-09-18 16:44:21 -0400

    Adds photoImposer.py and photoCoordinates.py and edits runner code

---
+++ backend/src/photoCoordinates.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 13:47:15 2020
+
+@author: himol7
+"""
+
+
+class photoCoordinates:
+    
+    allCoordinates = {}
+    
+    kickoff_coordinates = {
+        "R1" : (20, 140),
+        "R2" : (65, 140),
+        "R3" : (110, 140),
+        "R4" : (155, 140),
+        "R5" : (200, 140),
+        "PK" : (245, 140),
+        "L5" : (290, 140),
+        "L4" : (335, 140),
+        "L3" : (380, 140),
+        "L2" : (425, 140),
+        "L1" : (470, 140)
+        }
+    
+    kickoff_return_coordinates = {}
+    
+    punt_coordinates = {}
+        
+    punt_return_coordinates = {}
+    
+    fieldgoal_coordinates = {}
+    
+    fieldgoal_block_coordinates = {}
+    
+    def __init__(self):
+        self.allCoordinates['KICKOFF'] = self.kickoff_coordinates
+        self.allCoordinates['KICKOFF_RETURN'] = self.kickoff_return_coordinates
+        self.allCoordinates['PUNT'] = self.punt_coordinates
+        self.allCoordinates['PUNT_RETURN'] = self.punt_return_coordinates
+        self.allCoordinates['FIELDGOAL'] = self.fieldgoal_coordinates
+        self.allCoordinates['FIELDGOAL_BlOCK'] = self.fieldgoal_block_coordinates
+        
+        
+        
