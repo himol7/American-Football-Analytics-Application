41090b8f1b71a5fabd391b41102db927dcdd8f72
Amit Mandliya <amitmanldiya.davviet@gmail.com>
2020-09-17 23:53:38 -0400

    added logic to clean stale files

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,23 @@
 import os
+from os import listdir
+from os.path import isfile
 
-from flask import Flask, request, redirect, url_for, flash
+from flask import Flask, request, redirect, url_for, flash, send_file, send_from_directory, abort
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
 # upload folder for input files
 UPLOAD_FOLDER = 'input_files'
+
+# download folder for output files
+DOWNLOAD_FOLDER = 'output_files'
 
 # allowed extensions for the input files
 ALLOWED_EXTENSIONS = {'csv'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 CORS(app)
 
 
@@ -23,6 +30,7 @@ def validate_extension(filename):
 def upload_files():
     if request.method == 'POST':
         # logic to clean the folder
+        clean_directory(os.path.join(app.config['UPLOAD_FOLDER']))
 
         # check if the request comes with the input file
         if 'file' not in request.files:
@@ -48,10 +56,28 @@ def upload_files():
 
     return "Returning after Post"
 
+
+# this method contains the code to clean up the files from the directory if there are any
+def clean_directory(directory):
+    # get all the file names in files
+    for file in listdir(directory):
+        absolute_name = os.path.join(directory, file)
+        if isfile(absolute_name):
+            os.remove(absolute_name)
+
+
 @app.route('/analysis', methods=['GET'])
 def get_match_summary():
-    return ''
+    image_name = 'abc.png'
+    files = []
+    for file in listdir(app.config['DOWNLOAD_FOLDER']):
+        if isfile(os.path.join(app.config['DOWNLOAD_FOLDER'], file)):
+            files.extend(file)
 
+    try:
+        return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename=image_name, as_attachment=False)
+    except FileNotFoundError:
+        abort(404)
 
 
 if __name__ == '__main__':
