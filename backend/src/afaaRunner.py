c9fa082870001fef10b64125924eea73b950a8ce
himol7 <himolms@gmail.com>
2020-09-18 13:40:26 -0400

    Adds data analytics code and edits runner code

--- backend/src/afaaRunner.py
+++ backend/src/afaaRunner.py
@@ -8,22 +8,35 @@ Created on Thu Sep 17 19:36:42 2020
 
 from dataCollector import dataCollector 
 from playsSeperator import playsSeperator
+from dataAnalytics import dataAnalytics
 
 class afaaRunner:
     
     def  __init__(self):
         dc = dataCollector()
         ps = playsSeperator()
+        ds = dataAnalytics()
         
         team = "NCST"
         data = dc.readfile()    
         
-        print(data.head())
-        
         all_plays = ps.getDataframesByPlays(team, data)
-        print(all_plays)
         
+        offensive_plays = {"PUNT", "KICKOFF_RETURN", "FIELDGOAL"}
+        defensive_plays = {"PUNT_RETURN", "KICKOFF", "FIELDGOAL_BLOCK"}
         
-    
+        for playType, playData in all_plays.items():
+        
+            if playType in offensive_plays:
+                formations = playData["pff_OFFPLAYERS"]
+                ratings = playData["pff_OFFPLAYERSRATINGS"]
+                
+            elif playType in defensive_plays:
+                formations = playData["pff_DEFPLAYERS"]
+                ratings = playData["pff_DEFPLAYERSRATINGS"]
+                
+            countsAndRatings = ds.generateTotalCountsAndRatings(formations, ratings)
+            print(playType)    
+            print(countsAndRatings)
 
 a = afaaRunner()
