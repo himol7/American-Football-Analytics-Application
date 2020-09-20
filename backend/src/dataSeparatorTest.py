e67d5e10f758f6df2f2adbd1bfce7f6606d80b5e
Mukul Vashist <65409556+mukulv@users.noreply.github.com>
2020-09-20 18:52:16 -0400

    code review changes

--- backend/src/dataSeparatorTest.py
+++ backend/src/dataSeparatorTest.py
@@ -1,10 +1,10 @@
 from playsSeperator import playsSeperator
 from dataCollector import dataCollector
 
-dc = dataCollector()
-data = dc.readfile()
-ps = playsSeperator()
-plays = ps.getDataframesByPlays("NCST", data)
+data_collector = dataCollector()
+data = data_collector.readfile()
+play_separator = playsSeperator()
+plays = play_separator.getDataframesByPlays("NCST", data)
 
 def test02():
   for k, v in plays.items():
