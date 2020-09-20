0e658de45225c222ffb9b7e504569cda7afa08f4
KrisshaJ <35404860+KrisshaJ@users.noreply.github.com>
2020-09-20 19:06:23 -0400

    Update dataCollector_Test.py

--- backend/src/dataCollector_Test.py
+++ backend/src/dataCollector_Test.py
@@ -6,4 +6,4 @@ data_collector = dataCollector()
 
 
 def test_method():
-  assert data_collector.readfile() is not Non
+  assert data_collector.readfile('20191130 NCST K vs NCUN 31 PLAYS K.KR.P.PR.FG.FGB.csv') is not None
