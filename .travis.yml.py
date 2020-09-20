74aab2bd855a7d109cdc6bf7e79383731432208e
Mukul Vashist <65409556+mukulv@users.noreply.github.com>
2020-09-20 18:37:45 -0400

    Delete .travis.yml.py

--- .travis.yml.py
+++
@@ -1,8 +0,0 @@
-language: python
-python: "3.6"
-install:
-    - pip install -r requirements.txt
-script:
-    - pytest dataCollector_Test.py
-    - pytest dataSeparatorTest.py
-    - pytest dataAnalyticsTest.py
