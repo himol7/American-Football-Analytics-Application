93a14c54dfb8c927952900936ddf421f5950c811
tunveyyy <panditanvi@gmail.com>
2020-09-19 23:12:27 -0400

    upload photos

---
+++ vue-app/src/components/DisplayPhotos.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id = "app">
+        "qazxsw"
+        <img :src= "data" >
+        "qwerty"
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'DisplayPhotos',
+    data() {
+        return{
+            data: null
+        }
+    },
+    beforeMount(){
+        this.mounted()
+    },
+    methods: {
+        mounted(){
+            axios.get('http://localhost:5000/analysis')
+            .then((response) =>{
+                //let b64Response = btoa(unescape(encodeURIComponent(response.data)))
+                this.data = response.data
+
+
+            })
+            .catch(function (error){
+                console.log(error)
+            })
+        }
+    }
+}
+</script>
