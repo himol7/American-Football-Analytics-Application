2d249f15d39ed22915e877a3651f1ac660aa2139
KrisshaJ <35404860+KrisshaJ@users.noreply.github.com>
2020-09-21 14:30:52 -0400

    engine python

--- backend/src/DataCollector_Test.py
+++ backend/src/DataCollector_Test.py
@@ -6,7 +6,7 @@ data_collector = dataCollector()
 
 
 def test_method():
-  data = pd.read_csv("https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv")
+  data = pd.read_csv("https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv", engine = 'python')
   print(data)
   #y = "https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv"
   #x = data_collector.readfile(y)
