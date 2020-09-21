4c0c02e794ed8ad9b40ac6b8f1030bb40c0a7c8e
Amit Mandliya <amitmanldiya.davviet@gmail.com>
2020-09-21 19:53:08 -0400

    test files

---
+++ backend/src/DataSeparator_test.py
@@ -0,0 +1,11 @@
+from playsSeperator import playsSeperator
+from dataCollector import dataCollector
+
+data_collector = dataCollector()
+data = data_collector.readfile("NCSU.csv")
+play_separator = playsSeperator()
+plays = play_separator.getDataframesByPlays("NCST", data)
+
+def test02():
+  for k, v in plays.items():
+    assert v is not None
