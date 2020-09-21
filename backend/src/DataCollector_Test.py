a77ee343fd10985dbde9a091445a22881ff7576b
KrisshaJ <35404860+KrisshaJ@users.noreply.github.com>
2020-09-21 14:10:30 -0400

    Updated link

--- backend/src/DataCollector_Test.py
+++ backend/src/DataCollector_Test.py
@@ -6,6 +6,7 @@ data_collector = dataCollector()
 
 
 def test_method():
-  x = data_collector.readfile('NCSU.csv')
+  y = "https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv"
+  x = data_collector.readfile(y)
   print (x)
 #  assert data_collector.readfile('American-Football-Analytics-Application/backend/src/NCSU.csv') is not None
