826f4b44cc0ac6c135b43a3ffc4ead2d267a3e36
KrisshaJ <35404860+KrisshaJ@users.noreply.github.com>
2020-09-21 03:11:09 -0400

    Updated csv file name

--- backend/src/DataSeparatorTest.py
+++ backend/src/DataSeparatorTest.py
@@ -2,7 +2,7 @@ from playsSeperator import playsSeperator
 from dataCollector import dataCollector
 
 data_collector = dataCollector()
-data = data_collector.readfile("20191130 NCST K vs NCUN 31 PLAYS K.KR.P.PR.FG.FGB.csv")
+data = data_collector.readfile("NCSU.csv")
 play_separator = playsSeperator()
 plays = play_separator.getDataframesByPlays("NCST", data)
 
