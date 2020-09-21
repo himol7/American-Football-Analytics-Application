2d809edb96a97285d3e8cd5fbb228659ee56addc
KrisshaJ <35404860+KrisshaJ@users.noreply.github.com>
2020-09-21 14:00:56 -0400

    Path for csv

--- backend/src/DataCollector_Test.py
+++ backend/src/DataCollector_Test.py
@@ -6,4 +6,4 @@ data_collector = dataCollector()
 
 
 def test_method():
-  assert data_collector.readfile('NCSU.csv') is not None
+  assert data_collector.readfile('American-Football-Analytics-Application/backend/src/NCSU.csv') is not None
