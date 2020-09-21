ed4f1625117f77549d2ff6b292d837ddb129da19
KrisshaJ <35404860+KrisshaJ@users.noreply.github.com>
2020-09-21 14:20:29 -0400

    Trial run read csv

--- backend/src/DataCollector_Test.py
+++ backend/src/DataCollector_Test.py
@@ -1,12 +1,14 @@
 from dataCollector import dataCollector
-import pandas
+import pandas as pd
 import pytest
 
 data_collector = dataCollector()
 
 
 def test_method():
-  y = "https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv"
-  x = data_collector.readfile(y)
-  print (x)
+  data = pd.read_csv("NCSU.csv")
+  print(data)
+  #y = "https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv"
+  #x = data_collector.readfile(y)
+  #print (x)
 #  assert data_collector.readfile('American-Football-Analytics-Application/backend/src/NCSU.csv') is not None
