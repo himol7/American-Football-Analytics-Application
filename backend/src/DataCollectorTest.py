601e175182a1b3ea22e586de6de78b96c9f91fef
KrisshaJ <35404860+KrisshaJ@users.noreply.github.com>
2020-09-21 03:02:54 -0400

    Delete extra datacollector File

--- backend/src/DataCollectorTest.py
+++
@@ -1,9 +0,0 @@
-from dataCollector import dataCollector
-import pandas
-import pytest
-
-data_collector = dataCollector()
-
-
-def test_method():
-  assert data_collector.readfile("20191130 NCST K vs NCUN 31 PLAYS K.KR.P.PR.FG.FGB.csv") is not None
