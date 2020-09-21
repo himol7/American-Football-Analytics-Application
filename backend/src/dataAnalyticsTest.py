90acda08b6a13836e1a0acb309ddaffbd8539ef6
KrisshaJ <35404860+KrisshaJ@users.noreply.github.com>
2020-09-21 03:11:33 -0400

    Updated csv file name

--- backend/src/dataAnalyticsTest.py
+++ backend/src/dataAnalyticsTest.py
@@ -8,7 +8,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 da = dataAnalytics()
 dc = dataCollector()
-data = dc.readfile("20191130 NCST K vs NCUN 31 PLAYS K.KR.P.PR.FG.FGB.csv")
+data = dc.readfile("NCSU.csv")
 ps = playsSeperator()
 all_plays = ps.getDataframesByPlays("NCST", data)
 offensive_plays = {"PUNT", "KICKOFF_RETURN", "FIELDGOAL"}
