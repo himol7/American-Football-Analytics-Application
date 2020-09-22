6a1d2fec594e67bc5f20d3d248b51d0a15be9c43
Amit Mandliya <amitmandliya.davviet@gmail.com>
2020-09-21 20:47:11 -0400

    Update test_dataCollector.py

--- tests/test_dataCollector.py
+++ tests/test_dataCollector.py
@@ -6,4 +6,4 @@ data_collector = dataCollector()
 
 
 def test_method():
-  assert data_collector.readfile('https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv') is not None
+  assert data_collector.readfile('https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv?raw=true') is not None
