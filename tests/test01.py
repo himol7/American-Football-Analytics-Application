e3d64517185ae1fbebda3a8e5c64149242f48443
Amit Mandliya <amitmanldiya.davviet@gmail.com>
2020-09-21 20:39:43 -0400

    removed test folders

--- tests/test01.py
+++
@@ -1,9 +0,0 @@
-from dataCollector import dataCollector
-
-data_collector = dataCollector()
-
-def test_method():
-  data = pd.read_csv("https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv", engine = 'python')
-  print(data)
-  
-  
--- tests/test_dataCollector.py
+++
@@ -1,9 +0,0 @@
-from dataCollector import dataCollector
-import pandas
-import pytest
-
-data_collector = dataCollector()
-
-
-def test_method():
-  assert data_collector.readfile('https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv') is not None
