99a1b2269f3b7212e9b8264653cdf9eec084ba8c
Amit Mandliya <amitmandliya.davviet@gmail.com>
2020-09-21 21:12:32 -0400

    Update test_playsSeperator.py

--- tests/test_playsSeperator.py
+++ tests/test_playsSeperator.py
@@ -1,5 +1,5 @@
-from playsSeperator import playsSeperator
-from dataCollector import dataCollector
+from backend.src.playsSeperator import playsSeperator
+from backend.src.dataCollector import dataCollector
 
 data_collector = dataCollector()
 data = data_collector.readfile("https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv?raw=true")
